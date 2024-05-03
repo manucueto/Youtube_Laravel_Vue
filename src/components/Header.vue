4bb2a8c75435603885f29afd07ee38f345b6365c
Manuel Cueto <manuelcueto2004@gmail.com>
2024-05-03 13:27:59 -0400

    Instalando Tailwind y modificando estilos css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,11 +5,11 @@ export default {
 </script>
 
 <template>
-  <div class="container gradiente ">
-    <h1 class="">ComTube</h1>
+  <div class="flex justify-between fixed top-0 left-0 right-0 gradiante p-3 gradiente items-center ">
+    <h1 class="text-3xl text-white ml-5">ComTube</h1>
     <div class="separado">
-        <a href="#">Login</a>
-        <a href="#">Registrarse</a>
+        <a href="#" class="text-white">Login</a>
+        <a href="#" class="text-white">Registrarse</a>
     </div>
   </div>  
   
@@ -17,8 +17,9 @@ export default {
 
 <style>
     .gradiente{
-        /* Permalink - use to edit and share this gradient: https://colorzilla.com/gradient-editor/#ffffff+0,2be5d9+51,55db74+100 */
-        background: linear-gradient(to right, #ffffff 0%,#2be5d9 51%,#55db74 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+        /* Permalink - use to edit and share this gradient: https://colorzilla.com/gradient-editor/#55db74+0,2be5d9+51,55db74+100 */
+        background: linear-gradient(to right,  #55db74 0%,#2be5d9 51%,#55db74 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+
         position: fixed; /* Añade esta línea */
         top: 0; /* Añade esta línea */
         right: 0; /* Añade esta línea */
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// tailwind.config.js
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  // ... otras configuraciones
+}
+
+
+
